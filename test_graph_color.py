5bb9c640f627a05cd4d2fd47553bd353205d5338
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-03-17 19:21:26 +0200

    Explicitly pass color numbers, use stable order coloring.

--- test_graph_color.py
+++ test_graph_color.py
@@ -4,9 +4,9 @@ from graph import Ungraph
 from graph_color import RegColoring
 
 
-def assert_coloring(g, expected_coloring):
+def assert_coloring(g, colors, expected_coloring):
     org_g = deepcopy(g)
-    c = RegColoring(g, 10)
+    c = RegColoring(g, colors)
     assert c.simplify()
     c.select()
     print c.g
@@ -19,15 +19,15 @@ def assert_coloring(g, expected_coloring):
 
 def test_color_1_node():
     g = Ungraph.from_neigh_list({"a": []})
-    assert_coloring(g, [("a", 0)])
+    assert_coloring(g, 5, [("a", 0)])
 
 def test_color_2_nodes():
     g = Ungraph.from_neigh_list({"a": ["b"], "b": ["a"]})
-    assert_coloring(g, [("a", 1), ("b", 0)])
+    assert_coloring(g, 5, [("a", 1), ("b", 0)])
 
 def test_color_3_nodes():
     g = Ungraph.from_neigh_list({"a": ["b", "c"], "b": ["a", "c"], "c": ["a", "b"]})
-    assert_coloring(g, [('a', 2), ('b', 0), ('c', 1)])
+    assert_coloring(g, 5, [('a', 2), ('b', 1), ('c', 0)])
 
 def test_color_4_nodes_1():
     """
@@ -38,7 +38,7 @@ def test_color_4_nodes_1():
     g = Ungraph.from_neigh_list({
         "a": ["b", "d"], "b": ["a", "c"], "c": ["b", "d"], "d": ["a", "c"]
     })
-    assert_coloring(g, [('a', 1), ('b', 0), ('c', 1), ('d', 0)])
+    assert_coloring(g, 5, [('a', 1), ('b', 0), ('c', 1), ('d', 0)])
 
 def test_color_4_nodes_2():
     """
@@ -49,7 +49,7 @@ def test_color_4_nodes_2():
     g = Ungraph.from_neigh_list({
         "a": ["b", "c", "d"], "b": ["a", "c"], "c": ["a", "b", "d"], "d": ["a", "c"]
     })
-    assert_coloring(g, [('a', 2), ('b', 0), ('c', 1), ('d', 0)])
+    assert_coloring(g, 5, [('a', 2), ('b', 0), ('c', 1), ('d', 0)])
 
 def test_MCI_graph():
     "Graph from MCIiJ p.221"
@@ -69,5 +69,6 @@ def test_MCI_graph():
 
     g = Ungraph.from_neigh_list(NEIGHS)
     g.normalize()
-    assert_coloring(g,
-        [('b', 3), ('c', 1), ('d', 2), ('e', 2), ('f', 1), ('g', 2), ('h', 1), ('j', 0), ('k', 1), ('m', 0)])
+    assert_coloring(g, 4,
+        [('b', 1), ('c', 2), ('d', 2), ('e', 2), ('f', 3), ('g', 2), ('h', 0), ('j', 1), ('k', 0), ('m', 0)]
+    )
