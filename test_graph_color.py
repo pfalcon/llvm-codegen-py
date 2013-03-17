c22e4693eb7ed45230e82d9a324f18d208cfab66
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-03-17 18:46:29 +0200

    3-node coloring test and refactor.

--- test_graph_color.py
+++ test_graph_color.py
@@ -4,26 +4,26 @@ from graph import Ungraph
 from graph_color import RegColoring
 
 
-def test_color_1_node():
-    g = Ungraph.from_neigh_list({"a": []})
+def assert_coloring(g, expected_coloring):
     org_g = deepcopy(g)
-
     c = RegColoring(g, 10)
     assert c.simplify()
     c.select()
     print c.g
+    c.g.normalize()
     assert c.g == org_g
-    assert c.get_coloring() == [("a", 0)], c.get_coloring()
+    assert c.get_coloring() == expected_coloring, c.get_coloring()
     print "-----------"
+
+
+def test_color_1_node():
+    g = Ungraph.from_neigh_list({"a": []})
+    assert_coloring(g, [("a", 0)])
 
 def test_color_2_nodes():
     g = Ungraph.from_neigh_list({"a": ["b"], "b": ["a"]})
-    org_g = deepcopy(g)
+    assert_coloring(g, [("a", 1), ("b", 0)])
 
-    c = RegColoring(g, 10)
-    assert c.simplify()
-    c.select()
-    print c.g
-    assert c.g == org_g
-    assert c.get_coloring() == [("a", 1), ("b", 0)], c.get_coloring()
-    print "-----------"
+def test_color_3_nodes():
+    g = Ungraph.from_neigh_list({"a": ["b", "c"], "b": ["a", "c"], "c": ["a", "b"]})
+    assert_coloring(g, [('a', 2), ('c', 1), ('b', 0)])
