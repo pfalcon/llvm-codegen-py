a0a335245cbe4d1ec16d1c12ce3b6672770c8f9e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-03-17 17:42:57 +0200

    Add docstrings.

--- graph.py
+++ graph.py
@@ -1,4 +1,10 @@
 class Digraph(object):
+    """Directed graph class, can be created from 2 representations:
+    Edge list:
+        (Node1 -> Node2), (Node1 -> Node3), ...
+    Neighbors list:
+        { Node1: [Node2, Node3], ... }
+    """
 
     def __init__(self):
         self.edge_list = None
@@ -18,36 +24,42 @@ class Digraph(object):
         return g
 
     def empty(self):
+        "Return True if graph is empty, i.e. contains 0 nodes."
         if self.edge_list is not None:
             return len(self.edge_list) == 0
         else:
             return len(self.neigh_list) == 0
 
     def neighs(self, n):
+        "Return list of node's neighbors."
         if self.neigh_list is not None:
             return self.neigh_list[n]
         else:
             raise NotImplementedError
 
     def degree(self, n):
+        "Return node's degree, i.e. number of its neighbors."
         if self.neigh_list is not None:
             return len(self.neigh_list[n])
         else:
             raise NotImplementedError
 
     def get_edge_list(self):
+        "Return list of all edges in the graph."
         if self.edge_list is not None:
             return self.edge_list
         else:
             raise NotImplementedError
 
     def iter_nodes(self):
+        "Iterate over all nodes in the graph."
         if self.neigh_list is not None:
             return self.neigh_list.iterkeys()
         else:
             raise NotImplementedError
 
     def remove(self, n):
+        "Remove node and all its edges from the graph."
         if self.neigh_list is not None:
             del self.neigh_list[n]
             for nd, neighs in self.neigh_list.iteritems():
@@ -59,9 +71,11 @@ class Digraph(object):
             raise NotImplementedError
 
     def get_node_attr(self, node, attr):
+        "Get node attribute (arbitrary named values attached to node)."
         return self.node_attrs.setdefault(node, {}).get(attr)
 
     def set_node_attr(self, node, attr, val):
+        "Set node attribute (arbitrary named values attached to node)."
         self.node_attrs.setdefault(node, {})[attr] = val
 
     def __str__(self):
@@ -72,6 +86,9 @@ class Digraph(object):
 
 
 class Ungraph(Digraph):
+    """Undirected graph. Implementation is based on Digraph, and requires
+    edges of both direction between nodes to be recorded.
+    """
 
     def normalize_edge_repr(self):
         for n in self.edge_list:
@@ -89,7 +106,7 @@ class Ungraph(Digraph):
             self.normalize_neigh_repr()
 
     def add_with_neighs(self, node, neighs):
+        "Add node to the graph, together with edges to specified neighbors."
         for n in neighs:
             self.neigh_list[n].append(node)
         self.neigh_list[node] = neighs
-
