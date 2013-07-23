c432008d1509cf5dddc06779568dc304ec0c6731
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-24 02:48:41 +0300

    With bricmp opcode, can add proper appel-2ed-p204 test w/o artifacts.

--- tests/data/appel-2ed-p204-llvm-br.ll
+++ tests/data/appel-2ed-p204-llvm-br.ll
@@ -6,7 +6,8 @@ L1:
   %b = add i32 %a, 1
   %c = add i32 %c, %b
   %a = mul i32 %b, 2
-  bricmp lt i32 %a, 100, label %L1, label %b2
+  %btmp = icmp lt i32 %a, 100
+  br i1 %btmp, label %L1, label %b2
 
 b2:
   ret i32 %c
--- tests/test_liveness.py
+++ tests/test_liveness.py
@@ -7,7 +7,27 @@ from liveness import *
 
 datadir = os.path.dirname(__file__) + "/data/"
 
-def test_func_if():
+def test_appel_2ed_p204():
+    expected_ranges = [
+        set(["a", "c"]),
+        set(["b", "c"]),
+        set(["b", "c"]),
+        set(["a", "c"]),
+        set(["a", "c"]),
+        set([]),
+    ]
+    p = IRParser(open(datadir + "appel-2ed-p204.ll"))
+    mod = p.parse()
+    f = mod["func"]
+    l = Liveness(f)
+    live_ranges = l.live_out_map()
+    print live_ranges
+    for i, inst in enumerate(f.iter_insts()):
+        print i, inst
+        assert live_ranges[inst] == expected_ranges[i], "[%s] %s vs %s" % (inst, live_ranges[inst], expected_ranges[i])
+
+
+def test_appel_2ed_p204_llvm_variant():
     expected_ranges = [
         set(["a", "c"]),
         set(["b", "c"]),
@@ -17,7 +37,7 @@ def test_func_if():
         set(["a", "c"]),
         set([]),
     ]
-    p = IRParser(open(datadir + "appel-2ed-p204.ll"))
+    p = IRParser(open(datadir + "appel-2ed-p204-llvm-br.ll"))
     mod = p.parse()
     f = mod["func"]
     l = Liveness(f)
