e0a8f9469590307d5fdf86f69fade67ae6f6f7d4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-24 02:23:55 +0300

    Elaborate API.

--- liveness.py
+++ liveness.py
@@ -9,12 +9,12 @@ from llvm2py import *
 class Liveness(object):
 
     def __init__(self, func):
-        self.live_in = {}
-        self.live_out = {}
+        self._live_in = {}
+        self._live_out = {}
         for b in func:
             for i in b.instructions():
-                self.live_in[i] = set()
-                self.live_out[i] = set()
+                self._live_in[i] = set()
+                self._live_out[i] = set()
 
         changed = True
         iter = 1
@@ -25,20 +25,23 @@ class Liveness(object):
             for b in func:
                 for i in b.instructions():
                     print "Analyzing", i, "| def:", i.defines(), "| use:", i.uses()
-                    new_in = copy(self.live_in[i])
-                    new_out = copy(self.live_out[i])
+                    new_in = copy(self._live_in[i])
+                    new_out = copy(self._live_out[i])
                     new_in = i.uses() | (new_out - i.defines())
                     new_out = set()
                     print "Succ:", i.succ()
                     for s in i.succ():
-                        new_out |= self.live_in[s]
-                    if new_in != self.live_in[i] or new_out != self.live_out[i]:
+                        new_out |= self._live_in[s]
+                    if new_in != self._live_in[i] or new_out != self._live_out[i]:
                         changed = True
-                        self.live_in[i] = new_in
-                        self.live_out[i] = new_out
+                        self._live_in[i] = new_in
+                        self._live_out[i] = new_out
 
     def live_out_map(self):
-        return self.live_out
+        return self._live_out
+
+    def live_out(self, inst):
+        return self._live_out[inst]
 
 
 if __name__ == "__main__":
