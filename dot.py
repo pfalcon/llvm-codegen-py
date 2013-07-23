33e326879c3c8311d5570229457382100484b8b8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-24 01:46:09 +0300

    Allow to output to stream.

--- dot.py
+++ dot.py
@@ -1,7 +1,9 @@
+import sys
+
 # Simple module to output graph as .dot file, which can be viewed
 # with dot or xdot.py tools.
-def dot(graph):
-    print "digraph G {"
+def dot(graph, out=sys.stdout):
+    print >>out, "digraph G {"
     for fr, to in graph.iter_edges():
-        print '"%s" -> "%s"' % (fr, to)
-    print "}"
+        print >>out, '"%s" -> "%s"' % (fr, to)
+    print >>out, "}"
