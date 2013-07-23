774a0314cfa78a84ca08df39c646335a1ac7e85d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-24 02:23:19 +0300

    Support both directed and undirected graphs.

--- dot.py
+++ dot.py
@@ -2,8 +2,17 @@ import sys
 
 # Simple module to output graph as .dot file, which can be viewed
 # with dot or xdot.py tools.
-def dot(graph, out=sys.stdout):
-    print >>out, "digraph G {"
+def dot(graph, out=sys.stdout, directed=None):
+    if directed is None:
+        directed = graph.directed
+    if directed:
+        header = "digraph"
+        edge = "->"
+    else:
+        header = "graph"
+        edge = "--"
+
+    print >>out, "%s G {" % header
     for fr, to in graph.iter_edges():
-        print >>out, '"%s" -> "%s"' % (fr, to)
+        print >>out, '"%s" %s "%s"' % (fr, edge, to)
     print >>out, "}"
