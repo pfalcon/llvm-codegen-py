8ccdd9623d289e012286781f30b9f5dbde120e22
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-26 01:38:48 +0300

    Add interference testcase for appel_2ed_p221.

--- tests/test_interference.py
+++ tests/test_interference.py
@@ -17,3 +17,13 @@ def test_interference():
     ig = InterferenceGraph(f, l)
     #dot.dot(ig, open("interf.dot", "w"))
     assert ig == UngraphEdgeList.from_edge_list([("a", "c"), ("b", "c")])
+
+def test_appel_2ed_p221():
+    p = IRParser(open(datadir + "appel-2ed-p221.ll"))
+    mod = p.parse()
+    f = mod["func"]
+    l = Liveness(f)
+    ig = InterferenceGraph(f, l)
+    dot.dot(ig, open("interf.dot", "w"))
+    refg = dot.parse(open(datadir + "appel-2ed-p221.dot"), UngraphEdgeList())
+    assert ig == refg, "%s vs %s" % (ig, refg)
