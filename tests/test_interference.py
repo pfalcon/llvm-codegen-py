35896f5282dd9738817c9f3df9cd56dc3701f1a6
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-27 23:16:29 +0300

    Refactor and add dummy test for strlen.ll.nossa .

--- tests/test_interference.py
+++ tests/test_interference.py
@@ -9,21 +9,23 @@ import dot
 
 datadir = os.path.dirname(__file__) + "/data/"
 
-def test_interference():
-    p = IRParser(open(datadir + "appel-2ed-p204.ll"))
+def gen_interf(fname):
+    p = IRParser(open(datadir + fname))
     mod = p.parse()
-    f = mod["func"]
+    f = mod[0]
     l = Liveness(f)
     ig = InterferenceGraph(f, l)
-    #dot.dot(ig, open("interf.dot", "w"))
+    dot.dot(ig, open(fname + ".dot", "w"))
+    return ig
+
+def test_interference():
+    ig = gen_interf("appel-2ed-p204.ll")
     assert ig == UngraphEdgeList.from_edge_list([("a", "c"), ("b", "c")])
 
 def test_appel_2ed_p221():
-    p = IRParser(open(datadir + "appel-2ed-p221.ll"))
-    mod = p.parse()
-    f = mod["func"]
-    l = Liveness(f)
-    ig = InterferenceGraph(f, l)
-    dot.dot(ig, open("interf.dot", "w"))
+    ig = gen_interf("appel-2ed-p221.ll")
     refg = dot.parse(open(datadir + "appel-2ed-p221.dot"), UngraphEdgeList())
     assert ig == refg, "%s vs %s" % (ig, refg)
+
+def test_clang_strlen():
+    ig = gen_interf("strlen.ll.nossa")
