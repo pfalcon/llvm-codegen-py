aae7638d33a30c0aa61c9e1cc34ca30f816d32d3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-28 04:00:11 +0300

    Add regalloc testcase for Appel p.221.

--- tests/test_reg_alloc.py
+++ tests/test_reg_alloc.py
@@ -29,3 +29,7 @@ def reg_alloc(fname, K, expected_map):
 
 def test_appel_2ed_p204():
     reg_alloc("appel-2ed-p204.ll", 4, {'a': 1, 'c': 0, 'b': 1})
+
+def test_appel_2ed_p221():
+    expected = {'c': 2, 'b': 1, 'e': 2, 'd': 2, 'g': 2, 'f': 3, 'h': 0, 'k': 0, 'j': 1, 'm': 0}
+    reg_alloc("appel-2ed-p221.ll", 4, expected)
