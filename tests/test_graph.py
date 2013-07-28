4cfee76cdcbbc9c660a40d8b85e7c85a63830bb4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2013-07-28 03:18:34 +0300

    Update for new graph API.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,23 +1,21 @@
 from copy import deepcopy
 
-from graph import Digraph, Ungraph
+from graph import *
 
 
-def test_ungraph_edges_normalize():
-    g = Ungraph.from_edge_list([["a", "b"], ["d", "c"]])
-    g.normalize()
-    l = g.get_edge_list()
-    assert l == [["a", "b"], ["c", "d"]], l
+def test_ungraph_edge_normalize():
+    g = UngraphEdgeList.from_edge_list([["a", "b"], ["d", "c"]])
+    assert g == UngraphEdgeList.from_edge_list([["a", "b"], ["c", "d"]])
 
-def test_compare():
-    g1 = Ungraph.from_neigh_list({"a": []})
-    g2 = Ungraph.from_neigh_list({"a": []})
+def test_ungraph_adj_compare():
+    g1 = UngraphAdjList.from_neigh_list({"a": []})
+    g2 = UngraphAdjList.from_neigh_list({"a": []})
     assert g1 == g2
     g3 = deepcopy(g1)
     assert g1 == g3
 
-    h1 = Ungraph.from_neigh_list({"b": []})
+    h1 = UngraphAdjList.from_neigh_list({"b": []})
     assert g1 != h1
 
-    h2 = Ungraph.from_neigh_list({"a": ["b"]})
+    h2 = UngraphAdjList.from_neigh_list({"a": ["b"]})
     assert g1 != h2
